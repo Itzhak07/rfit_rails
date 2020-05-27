3d6614a6dcce70ee698ff309c62e8651e2a1504e
Itzhak Tzoref <itzhak@valu.co.il>
2020-05-27 11:06:47 +0300

    i wrote routes as they should be

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  resources :users, only:[:index, :create, :show ]
+  resources :users, only:[:index, :create, :show ] do
+    post 'login', on: :collection
+    put 'update', on: :collection
+  end
   resources :clients, only: [:index, :create, :update]
   resources :workouts, only: [:index, :create, :destroy, :update]
   resources :movements
   resources :work_sets, only:[:index, :show, :update, :destroy]
 
   root 'home#index'
-
-  #update user info
-  put '/users/update' => 'users#update'
-
-  #login
-  post '/users/login' => 'users#login'
 
 end
